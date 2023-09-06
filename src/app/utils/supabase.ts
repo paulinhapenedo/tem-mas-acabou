530fea74ea2d10b123a24012ad329916ca6eb85e
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-05 22:13:48 -0300

    fix: remove supabase server client from the utils

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,18 +1,11 @@
-import { cookies } from 'next/headers';
-import {
-  createServerComponentClient,
-  createPagesBrowserClient,
-} from '@supabase/auth-helpers-nextjs';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+
+import { Database } from '~/types/supabase';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const createSupabaseServerClient = () =>
-  createServerComponentClient({
-    cookies,
-  });
-
-export const createSupabaseBrowserClient = createPagesBrowserClient({
+export const createSupabaseBrowserClient = createPagesBrowserClient<Database>({
   supabaseUrl,
   supabaseKey: supabaseAnonKey,
 });
