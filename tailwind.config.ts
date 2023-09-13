c977223db3fd95863dc0295809df52d70b5b0503
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-13 16:25:22 -0300

    chore: fix font-family issues and remove dark mode CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
     container: {
       center: true,
       padding: '2rem',
