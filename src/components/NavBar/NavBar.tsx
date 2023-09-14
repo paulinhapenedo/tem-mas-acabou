751e18e3b6cad57f314523120450a0765caeaf8e
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-14 16:26:58 -0300

    chore: update NavBar spacing to avoid clashing on mobile

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -12,7 +12,7 @@ export default async function NavBar() {
   } = await supabase.auth.getUser();
 
   return (
-    <nav className="flex flex-shrink-0 flex-nowrap justify-end px-4 pt-6">
+    <nav className="flex flex-shrink-0 flex-nowrap justify-end px-4 pt-6 mb-6 lg:mb-0">
       <a href="#skip" className="sr-only focus:not-sr-only">
         Pular para o conteúdo
       </a>
