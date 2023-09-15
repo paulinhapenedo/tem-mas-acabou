d9384705c63a1341ab4152f9a29892b4d00764c7
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-15 19:35:37 -0300

    fix: move middleware to the correct folder

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,18 @@ import type { NextRequest } from 'next/server';
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
-  await supabase.auth.getSession();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session && req.nextUrl.pathname !== '/unauthenticated') {
+    return NextResponse.redirect(new URL('/unauthenticated', req.url));
+  }
+
   return res;
 }
+
+export const config = {
+  matcher: ['/', '/profile/:path*'],
+};
