c840955fba712120f429762a810bf7c85669c0d0
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-20 19:45:01 -0300

    feat: add end-to-end testing to signup flow

---
+++ src/e2e/signup-flow.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test';
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const ALLOWED_EMAIL = process.env.NEXT_PUBLIC_USER1 || '';
+const AUTH_URL = `${SUPABASE_URL}/auth/v1/`;
+
+test.describe('Sign-up flow', () => {
+  test(`user should see private beta message if email isn't in the allowed list`, async ({
+    page,
+  }) => {
+    await page.goto('/unauthenticated');
+
+    await page.getByPlaceholder('Digite o seu email').click();
+    await page.getByPlaceholder('Digite o seu email').fill('email@email.com');
+    await page.getByPlaceholder('Digite uma senha segura').click();
+    await page.getByPlaceholder('Digite uma senha segura').fill('s3cur3pwd');
+    await page.getByRole('button', { name: 'Cadastrar' }).click();
+
+    const feedbackTitle = await page
+      .getByRole('alert')
+      .getByRole('paragraph')
+      .first()
+      .innerText();
+    const feedbackDescription = await page
+      .getByRole('alert')
+      .getByRole('paragraph')
+      .last()
+      .innerText();
+
+    expect(feedbackTitle).toBe('Desculpe o inconveniente! 🙈');
+    expect(feedbackDescription).toBe(
+      'Esse é um app para estudo, então o registro está limitado.',
+    );
+  });
+
+  test(`user should see error message if something went wrong`, async ({
+    page,
+  }) => {
+    await page.route(`${AUTH_URL}/signup`, async (route) => {
+      await route.fulfill({ status: 400 });
+    });
+
+    await page.goto('/unauthenticated');
+
+    await page.getByPlaceholder('Digite o seu email').click();
+    await page.getByPlaceholder('Digite o seu email').fill(ALLOWED_EMAIL);
+    await page.getByPlaceholder('Digite uma senha segura').click();
+    await page.getByPlaceholder('Digite uma senha segura').fill('s3cur3pwd');
+    await page.getByRole('button', { name: 'Cadastrar' }).click();
+
+    const feedbackTitle = await page
+      .getByRole('alert')
+      .getByRole('paragraph')
+      .first()
+      .innerText();
+    const feedbackDescription = await page
+      .getByRole('alert')
+      .getByRole('paragraph')
+      .last()
+      .innerText();
+
+    expect(feedbackTitle).toBe('Erro');
+    expect(feedbackDescription).toBe(
+      'Usuário já existe. Por favor, tente fazer login.',
+    );
+  });
+});
