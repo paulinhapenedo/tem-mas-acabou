f9e11001283632bc9e2e1c83b132c7dab7ef7c96
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-20 10:50:38 -0300

    feat: Add error and not found pages (#7)
    * feat: add error page
    
    * chore: add global-error and not-found pages
    
    * chore: add placeholder to next-images

---
+++ src/app/global-error.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import { useEffect } from 'react';
+
+import { Button } from '~/ui/button';
+import { Typography } from '~/ui/typography';
+
+const IMAGE_URL = '/entei-error.png';
+const IMAGE_SIZE = 200;
+
+const strings = {
+  title: 'Algo de errado não está certo!',
+  description: `Sabemos que pode ser desconsertante estar aqui. Por isso, o Entei está
+  aqui para te guiar em segurança para a página inicial.`,
+  altImage: `Ilustração do Pokémon Entei em referência ao meme 'Tá tudo bem agora'`,
+  button: `Clique aqui para atualizar a página`,
+};
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <html lang="pt-br">
+      <body className={`${inter.variable} font-sans`}>
+        <main className="grid place-content-center md:place-items-center gap-16 h-[85dvh] max-w-prose m-auto">
+          <div>
+            <Typography.H1>{strings.title}</Typography.H1>
+            <Typography.P>{strings.description}</Typography.P>
+            <Image
+              className="mx-auto my-4"
+              src={IMAGE_URL}
+              width={IMAGE_SIZE}
+              height={IMAGE_SIZE}
+              alt={strings.altImage}
+              placeholder="blur"
+            />
+          </div>
+          <Button onClick={reset}>{strings.button}</Button>
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Typography } from '~/ui/typography';
+
+const IMAGE_URL = '/entei-error.png';
+const IMAGE_SIZE = 200;
+
+const strings = {
+  title: 'Algo de errado não está certo!',
+  description: `Sabemos que pode ser desconsertante estar aqui. Por isso, o Entei está
+  aqui para te guiar em segurança para a página inicial.`,
+  altImage: `Ilustração do Pokémon Entei em referência ao meme 'Tá tudo bem agora'`,
+  button: `Clique aqui para redirecionar para a página inicial`,
+};
+
+export default function NotFound() {
+  return (
+    <div className="grid place-content-center md:place-items-center gap-16 h-[85dvh] max-w-prose m-auto">
+      <div>
+        <Typography.H1>{strings.title}</Typography.H1>
+        <Typography.P>{strings.description}</Typography.P>
+        <Image
+          className="mx-auto my-4"
+          src={IMAGE_URL}
+          width={IMAGE_SIZE}
+          height={IMAGE_SIZE}
+          alt={strings.altImage}
+          placeholder="blur"
+        />
+      </div>
+      <Link className="text-foreground underline-offset-4 underline" href="/">
+        {strings.button}
+      </Link>
+    </div>
+  );
+}
