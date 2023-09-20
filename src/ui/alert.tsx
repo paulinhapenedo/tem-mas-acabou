10309128a387452e1a239eb75c577dce166100b1
Paula Penedo <paulinhapenedo@gmail.com>
2023-09-20 19:41:04 -0300

    fix: update Alert component to follow web semantics

--- src/ui/alert.tsx
+++ src/ui/alert.tsx
@@ -36,7 +36,7 @@ const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h5
+  <p
     ref={ref}
     className={cn('mb-1 font-medium leading-none tracking-tight', className)}
     {...props}
@@ -48,7 +48,7 @@ const AlertDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <div
+  <p
     ref={ref}
     className={cn('text-sm [&_p]:leading-relaxed', className)}
     {...props}
