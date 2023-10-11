d64396c94b0345f89a9bd03abee39f57c79582d0
Paula Penedo <paulinhapenedo@gmail.com>
2023-10-11 00:44:31 -0300

    feat: use PageWrapper on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
-import { SidebarMobile, SidebarNav } from '~/components/Sidebar';
+import { PageWrapper } from '~/components/PageWrapper';
 import { createServerSupabaseClient } from '~/services/supabase-server';
 import { Button } from '~/ui/button';
 import { Typography } from '~/ui/typography';
@@ -17,34 +17,25 @@ export default async function Home() {
   }
 
   return (
-    <>
-      <h1 className="text-2xl font-bold tracking-tight">Bem vinda!</h1>
-      <Typography.Lead>O que deseja fazer?</Typography.Lead>
-
-      <SidebarMobile />
-      <div className="grid with-sidebar-mobile lg:grid-cols-with-sidebar lg:pt-6">
-        <div className="hidden lg:block">
-          <SidebarNav />
-        </div>
-        <section className="py-4 lg:px-4 lg:col-start-3 lg:col-span-4">
-          <Typography.H3>Últimas listas criadas</Typography.H3>
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            <li>09/10/2023 - Hortifruti</li>
-            <li>23/09/2023 - Mercado</li>
-            <li>10/09/2023 - Hortifruti</li>
-          </ul>
-          <Button className="w-full">Ver</Button>
-        </section>
-        <section className="py-4 lg:px-4 lg:col-span-4 lg:col-start-8">
-          <Typography.H3>Últimos produtos</Typography.H3>
-          <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-            <li>Suco de laranja Oba 1L</li>
-            <li>Batata asterix</li>
-            <li>Tomate italiano</li>
-          </ul>
-          <Button className="w-full">Ver</Button>
-        </section>
-      </div>
-    </>
+    <PageWrapper title="Bem vinda!" subtitle="O que deseja fazer?">
+      <section className="py-4 lg:px-4 lg:col-start-3 lg:col-span-4">
+        <Typography.H3>Últimas listas criadas</Typography.H3>
+        <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+          <li>09/10/2023 - Hortifruti</li>
+          <li>23/09/2023 - Mercado</li>
+          <li>10/09/2023 - Hortifruti</li>
+        </ul>
+        <Button className="w-full">Ver</Button>
+      </section>
+      <section className="py-4 lg:px-4 lg:col-span-4 lg:col-start-8">
+        <Typography.H3>Últimos produtos</Typography.H3>
+        <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+          <li>Suco de laranja Oba 1L</li>
+          <li>Batata asterix</li>
+          <li>Tomate italiano</li>
+        </ul>
+        <Button className="w-full">Ver</Button>
+      </section>
+    </PageWrapper>
   );
 }
