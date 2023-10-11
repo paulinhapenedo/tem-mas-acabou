8fdc95af277fc7bba9006ba5cd83d191f8596aae
Paula Penedo <paulinhapenedo@gmail.com>
2023-10-11 00:45:14 -0300

    wip: start working on the products endpoint

---
+++ src/app/products/page.tsx
@@ -0,0 +1,26 @@
+import { PageWrapper } from '~/components/PageWrapper';
+import { createServerSupabaseClient } from '~/services/supabase-server';
+
+import RealtimeProducts from './realtime';
+
+export default async function Products() {
+  const supabase = createServerSupabaseClient();
+
+  const { data } = await supabase.from('products').select();
+
+  const showEmptyState = !data?.length;
+  const hasData = !!data?.length;
+
+  // show empty state
+  const emptyState = () => <div>Empty state</div>;
+
+  return (
+    <PageWrapper
+      title="Produtos"
+      subtitle={`${data?.length} produtos cadastrados`}
+    >
+      {showEmptyState && emptyState()}
+      {hasData && <RealtimeProducts productsFromServer={data} />}
+    </PageWrapper>
+  );
+}
