254be64d975c70ed0a886691821eaf92a1469af4
Paula Penedo <paulinhapenedo@gmail.com>
2024-09-23 23:17:34 -0300

    chore: remove husky prepare and unused component

--- src/app/listas/criar/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PageWrapper } from '~/components/PageWrapper';
-
-export default function CreateNewListPage() {
-  return (
-    <PageWrapper title="Criar lista" subtitle="">
-      <section>Criar lista</section>
-    </PageWrapper>
-  );
-}
